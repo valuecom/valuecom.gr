80dc81006d2d0c34e468002aa74b34d711a74004
valuecom <devs@valuecom.gr>
2023-03-20 14:05:53 +0200

    changes

--- build.php
+++ build.php
@@ -1,6 +1,7 @@
 <?php
 
 $html = file_get_contents('http://localhost/GitHubVC/valuecom.gr/src/index.php');
- 
-
 file_put_contents('./public/index.html', $html);
+
+$html = file_get_contents('http://localhost/GitHubVC/valuecom.gr/src/we-believe.php');
+file_put_contents('./public/we-believe.html', $html);
