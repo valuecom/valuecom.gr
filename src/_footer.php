25e7b9c0924e5cc42fbea3da9dc96e54f98dcbdd
valuecom <devs@valuecom.gr>
2023-03-21 15:09:06 +0200

    15:08

--- src/_footer.php
+++ src/_footer.php
@@ -1,4 +1,4 @@
-<footer id="footer" class="py-5">
+<footer id="footer" class="py-5 mt-5">
     <div class="container-xxl d-flex justify-content-end">
         <ul class="m-0 p-0">
             <li class="">
--- src/_header.php
+++ src/_header.php
@@ -35,7 +35,7 @@
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="#">Contact</a>
-                  </li>
+                  </li> 
                 </ul>
               </div>
             </div>
