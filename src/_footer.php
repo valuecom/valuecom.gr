e659410c48d3a9bc664dc0f871e1cbc748cb2f7d
valuecom <devs@valuecom.gr>
2023-03-21 15:20:53 +0200

    Update _footer.php

--- src/_footer.php
+++ src/_footer.php
@@ -1,4 +1,4 @@
-<footer id="footer" class="py-5 mt-5">
+<footer id="footer" class="position-absolute bottom-0 start-0 end-0 py-5 mt-5">
     <div class="container-xxl d-flex justify-content-end">
         <ul class="m-0 p-0">
             <li class="">
