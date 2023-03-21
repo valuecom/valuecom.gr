f633618e1a8e347eccc8dab16a91faef3097afd2
valuecom <devs@valuecom.gr>
2023-03-21 12:14:08 +0200

    n

---
+++ src/_header.php
@@ -0,0 +1,44 @@
+<header class="d-flex align-items-center">
+    <div class="container-xxl ">
+      <div class="row align-items-center">
+        <div class="col-2">
+          <div class="logo-wrap">
+            <a href="#">
+              <img src="../assets/images/logo.svg" alt="Valuecom">
+            </a>
+          </div>
+        </div>
+        <div class="col-10">
+          <nav class="navbar navbar-expand-lg justify-content-end">
+            <div class="">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="#">Clients</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Work</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="#">Culture</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">The team</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Careers</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
