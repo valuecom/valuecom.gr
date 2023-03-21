28579299533c456aa52d3099b9b27e11c9b59b9e
valuecom <devs@valuecom.gr>
2023-03-21 12:27:30 +0200

    Update we-believe.php

--- src/we-believe.php
+++ src/we-believe.php
@@ -17,7 +17,23 @@
     <!-- page title -->
     <section class="page-title-section">
       <div class="container-xxl">
-        
+        <div class="page-title-wrap my-5 px-5">
+          <h1 class="page-title fs-1 fw-normal text-center text-lg-start">We believe</h1>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container-xxl d-flex justify-content-center pb-5">
+        <img src="../assets/images/we-believe.png">
+      </div>
+    </section>
+    <section>
+      <div class="container-xxl pb-5 px-5">
+        <div class="px-md-5 mx-lg-5">
+          <p>To make great work you need to create an environment that fosters greatness. A place where talented individuals are empowered to create and deliver their best.</p>
+          <p>We constantly strive to make VALUECOM this sort of place. How? Through our culture! Our way of work is "excellence" - excellence means we give it our best, our finest shot. Our effort is focused, consistent, resilient, intelligent, thoughtful & passionate.</p>
+          <p>We work with respect & integrity, transparency & accountability. We believe in team effort & invest in making our team better. We believe in hard work and a healthy, balanced lifestyle.</p>
+        </div>
       </div>
     </section>
 
