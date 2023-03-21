bf8834e243b71400f9ac7eef58478559f4713017
valuecom <devs@valuecom.gr>
2023-03-21 16:49:16 +0200

    import favicon & change site title

--- src/_header.php
+++ src/_header.php
@@ -6,9 +6,10 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
+  <title>VALUECOM | Fullstack Marketing Agency</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   <link rel="stylesheet" href="../assets/css/style.css">
+  <link rel="icon" href="../assets/images/favicon.png" sizes="32x32">
 </head>
 
 <body class="d-flex flex-column">
