132011833e37ad0ccd0aaa5b1a890ded998f094d
valuecom <devs@valuecom.gr>
2023-03-21 12:16:15 +0200

    Delete _page-title.php

--- src/_page-title.php
+++
@@ -1,7 +0,0 @@
-<div class="page-title-section">
-    <div class="container-xxl">
-        <div class="page-title-wrap my-5 px-5">
-            <h1 class="page-title fs-1 fw-normal">We believe</h1>
-        </div>
-    </div>
-</div>
